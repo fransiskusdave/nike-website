cae27fa2ab6e83b541be6ef4c1f74a8a509b11c8
Fransiskus Dave <davefransiskus@gmail.com>
2024-05-10 17:55:30 +0700

    added file named servicecard.jsx and finish it

---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,17 @@
+const ServiceCard = ({ imgURL, label, subtext }) => {
+  return (
+    <div className="flex-1 sm:w-[350px] sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16">
+      <div className="w-11 h-11 flex justify-center items-center bg-coral-red rounded-full">
+        <img src={imgURL} alt={label} className="w-6 h-6 object-contain" />
+      </div>
+      <h3 className="mt-5 font-palanquin text-3xl leading-normal font-bold">
+        {label}
+      </h3>
+      <p className="mt-3 font-montserrat break-words leading-normal text-slate-gray text-lg">
+        {subtext}
+      </p>
+    </div>
+  );
+};
+
+export default ServiceCard;
