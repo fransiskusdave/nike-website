ecfdf71105cf2a194e4bb88a83a640b64f3d7e92
Fransiskus Dave <davefransiskus@gmail.com>
2024-05-10 17:37:44 +0700

    completing superquality section

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,5 +1,39 @@
+import Button from "../components/Button";
+import { shoe8 } from "../assets/images";
+
 const SuperQuality = () => {
-  return <div>SuperQuality</div>;
+  return (
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-col flex-1">
+        <h2 className="text-4xl font-palanquin capitalize font-bold lg:max-w-lg">
+          We Provide You <span className="text-coral-red">Super</span>{" "}
+          <span className="text-coral-red">Quality</span> Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Ensuring premium comfort and style, our meticulously crafted footweaar
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and excellence ensures your satisfaction.
+        </p>
+        <div className="mt-11">
+          <Button label="View Details" />
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center flex-1">
+        <img
+          src={shoe8}
+          alt="shoe8"
+          className="object-contain w-[570px] h-[522px]"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default SuperQuality;
