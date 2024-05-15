551f278a957a379c3f979db21f6ac6018653b3c4
Fransiskus Dave <davefransiskus@gmail.com>
2024-05-15 13:18:53 +0700

    completing subscribe section

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,5 +1,23 @@
+import Button from "../components/Button";
+
 const Subscribe = () => {
-  return <div>Subscribe</div>;
+  return (
+    <section
+      className="max-container w-full flex justify-between items-center max-lg:flex-col gap-10"
+      id="contact-us"
+    >
+      <h3 className="text-4xl font-bold lg:max-w-md leading-[68px] font-palanquin">
+        Sign Up From<span className="text-coral-red"> Updates </span>&
+        Newsletter
+      </h3>
+      <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
+        <input type="text" placeholder="Subscribe@nike.com" className="input" />
+        <div className="flex max-sm:justify-end items-center max-sm:w-full">
+          <Button label="Sign Up" fullWidth />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Subscribe;
